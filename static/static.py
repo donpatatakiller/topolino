037d9082bf7d050cb57aeb33cbd1c1a5f2b18e4b
donpatatakiller <dona.vita@hotmail.it>
2025-10-09 12:18:38 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,6 +1,6 @@
-HTML = """ 
+HTML = """
 Don’t care about them all Cause all I want is to
-be loved And all I care about is you You’re stuck
+be loved And all I care about finding is you You’re stuck
 on me like a tattoooo No, I don’t care about
 the pain I’ll walk through fire and through rain
 """
